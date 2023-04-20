5c56c1e3ff4d5c2e0c43227c5977439d29b9f5bf
Solveiga <solveiga.galatiltyte@bjss.com>
2023-04-20 15:26:37 +0100

    Visual feedback for correct and wrong answers

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const correctAnswer = ["A", "A", "A", "A", "A"];
-
 const form = document.querySelector(".quiz-form");
-const result = document.querySelector(".result") 
+const result = document.querySelector(".result");
+const questions = document.querySelectorAll(".question"); //returns a NodeList
 
 form.addEventListener("submit", event => {
     event.preventDefault();
     
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value]; //capturing all selected answers in the form
-
-    userAnswers.forEach((answer, index) => {
+   userAnswers.forEach((answer, index) => {
         if (answer === correctAnswer[index]) {
             score++;
+            questions[index].classList.add("correct")           
+        } else {
+            questions[index].classList.add("wrong")
         }
     });
 
